f8e9cb4caab0d51a867f151f22fca2ecf164b829
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-02-20 10:05:00 -0600

    Instalacion y configuracion de TanStack Query - Devtools y Axios

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,8 +1,19 @@
 import { RouterProvider } from "react-router"
 import { appRouter } from "./router/app.router"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+const queryClient = new QueryClient;
+
 export const TesloShopApp = () => {
     return (
-        <RouterProvider router={appRouter} />
+
+        <QueryClientProvider client={queryClient}>
+
+            {/* The rest of your application */}
+            <RouterProvider router={appRouter} />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     )
 }
