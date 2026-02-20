f837028826cccb2fb912441b91fb9e049530fbde
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-02-20 12:01:52 -0600

    Accion - getProductsAction

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -2,8 +2,11 @@ import { CustomPagination } from "@/components/custom/CustomPagination"
 import { products } from "@/mocks/products.mock"
 import { CustomJombotron } from "@/shop/components/CustomJumbotron"
 import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useProducts } from "@/shop/hooks/useProducts"
 
 export const HomePage = () => {
+    const { data } = useProducts();
+
     return (
         <>
             <CustomJombotron title="All products" description="Minimalist and elegant clothing inspired by Tesla's futuristic design. Premium quality for timeless style." />
